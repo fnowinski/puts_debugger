dce9e8d9efb91f632a161e01529fa29f0bd784cf
Frank Nowinski <frank@tunecore.com>
2019-10-07 12:16:51 -0400

    Output caller

--- lib/tc_putter.rb
+++ lib/tc_putter.rb
@@ -3,7 +3,10 @@ require "tc_putter/version"
 module TcPutter
   def self.put(&block)
     puts "#" * 60
+    puts "#" * 60
     puts yield
+    puts caller.grep_v(/gem/)
+    puts "#" * 60
     puts "#" * 60
   end
 end
--- lib/tc_putter/version.rb
+++ lib/tc_putter/version.rb
@@ -1,3 +1,3 @@
 module TcPutter
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
